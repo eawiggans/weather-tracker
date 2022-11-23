a5ec02087deec558d23bec5280f1c158ce3c2519
Emma Wiggans <ea.wiggans@gmail.com>
2022-11-22 23:45:44 -0500

    saved city buttons working, forecast and current display all required

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,16 @@
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
 var fetchButton = document.getElementById("search-btn");
 var inputValue = document.getElementById("search-input");
+
 fetchButton.addEventListener("click", function(event) {
     event.preventDefault();
     getApi(inputValue.value);
 });
-// var formSubmitHandler = function (event) {
-//     event.preventDefault();
-//     var cityName = city.value.trim();
-// }
 
 
 function getApi(city) {
     var getLatLong = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=e893f77c4ef5c1780913960b49428199";
     console.log(getLatLong);
-    // var getLatLong = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=e893f77c4ef5c1780913960b49428199";
 ;
     fetch(getLatLong)
         .then(function (response) {
@@ -29,7 +25,7 @@ function getApi(city) {
             console.log(error);
         })
 
-    // 
+
 }
 function getCityData(data) { 
     var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=" + data.lat + "&lon=" + data.lon + "&appid=e893f77c4ef5c1780913960b49428199";
@@ -49,34 +45,48 @@ function getCityData(data) {
 }
 
 function oneDayDisplay(cityData, cityName) {
-    let currentCard = document.getElementById("current-weather");
-    let card = document.createElement("div");
+    var currentCard = document.getElementById("current-weather");
+    var card = document.createElement("div");
+    var cardName = document.createElement("div");
+    var cardBody = document.createElement("div");
+    var iconImg = document.createElement("img");
+    while (currentCard.firstChild) {
+        currentCard.removeChild(currentCard.firstChild);
+      }
     card.classList.add("card");
-    let cardName = document.createElement("div");
-    cardName.classList.add("card-title");
-    let cardBody = document.createElement("div");
+    cardName.classList.add("card-header");
     cardBody.classList.add("card-body");
     cardName.textContent = cityName;
-    cardBody.innerHTML = `temp: ${cityData.main.temp}`;
+    cardBody.innerHTML = `temp: ${cityData.main.temp} humidity: ${cityData.main.humidity} wind speed: ${cityData.wind.speed}`;
+    iconImg.setAttribute("src", `http://openweathermap.org/img/wn/${cityData.weather[0].icon}.png`);
     card.append(cardName);
+    card.append(iconImg);
     card.append(cardBody);
     currentCard.append(card);
+    
+    localStorage.setItem(cityName, JSON.stringify(cityName));
 }
 
 function fiveDayDisplay(weatherData) {
     var fiveDays = document.getElementById("five-day-container");
+    while (fiveDays.firstChild) {
+        fiveDays.removeChild(fiveDays.firstChild);
+      }
     for (let i = 0; i < weatherData.list.length; i++) {
         var element = weatherData.list[i];
         if (element.dt_txt.includes("06:00:00")) {
             let card = document.createElement("div");
-            card.classList.add("card");
+            card.classList.add("card", "col-2");
             let cardName = document.createElement("div");
-            cardName.classList.add("card-title");
+            cardName.classList.add("card-header");
             let cardBody = document.createElement("div");
             cardBody.classList.add("card-body");
             cardName.textContent = element.dt_txt;
-            cardBody.innerHTML = element.main.temp;
+            cardBody.innerHTML = `temp: ${element.main.temp} humidity: ${element.main.humidity} wind speed: ${element.wind.speed}`;
+            let cardImg = document.createElement("img");
+            cardImg.setAttribute("src", `http://openweathermap.org/img/wn/${element.weather[0].icon}.png`);
             card.append(cardName);
+            card.append(cardImg);
             card.append(cardBody);
             fiveDays.append(card);
         
@@ -85,6 +95,34 @@ function fiveDayDisplay(weatherData) {
     }
 
 }
+
+
+
+function renderPast() {
+    var savedCard = document.getElementById("past-searches");
+    for (let i = 0; i < window.localStorage.length; i++) {
+        var keys = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+        var savedBtn = document.createElement("button");
+        savedBtn.classList.add("btn", "saved-btn", "btn-outline-primary", "col");
+        savedBtn.textContent = keys;
+        savedCard.append(savedBtn);
+    }
+    
+}
+
+renderPast();
+
+var fetchSaved = $(".saved-btn");
+
+// why did this not work with vanilla javascript??????
+
+fetchSaved.on("click", function(event) {
+    var savedCity = event.target.textContent;
+    getApi(savedCity);
+});
+
+
 
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
